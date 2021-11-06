93d64448c49dcd9e86800755fd2603f1c2d0187b
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2021-11-06 09:31:16 +0200

    feat: Add theme layout and client side libraries

--- Hydra.Server.Auth/Controllers/HomeController.cs
+++ Hydra.Server.Auth/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Hydra.Server.Auth.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Hydra.Server.Auth.Models;
+using System.Diagnostics;
 
 namespace Hydra.Server.Auth.Controllers
 {
@@ -18,17 +15,13 @@ namespace Hydra.Server.Auth.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
