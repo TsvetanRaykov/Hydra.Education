6639b4ddb527a453f43d3ac8ae6f854ac246f6dc
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2022-01-15 15:54:18 +0200

    feat: Add Hydra.Module.Video.Api to the repository.

---
+++ Hydra.Module.Video.Api/Startup.cs
@@ -0,0 +1,79 @@
+namespace Hydra.Module.Video.Api
+{
+    using Backend;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hydra.Module.Video.Api", Version = "v1" });
+            });
+
+            services.AddHydraModuleVideo(options =>
+            {
+                // Optional when used outside of the cookie domain
+                options.AddHydraModuleJwtTokenAuthentication(services);
+            });
+
+            services.AddAuthentication();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                    builder.SetIsOriginAllowed(_ => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hydra.Module.Video.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseHydraModuleVideo(opt =>
+            {
+                opt.StaticFilesLocation = Configuration["FileStoragePath"];
+            });
+
+            app.UseAuthorization();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
---
+++ Hydra.Module.Video.Api/StudentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Hydra.Module.Video.Api;
+
+public class StudentDto
+{
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string IdentityNumber { get; set; }
+}
